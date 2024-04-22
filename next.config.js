d07617b765b873ce60a4aaf1dbc84ad32e13b955
jasonlin1993 <ninjachen.lin@gmail.com>
2024-04-22 16:06:26 +0800

    解決 Google 登入遇到 Cors 問題

--- next.config.js
+++ next.config.js
@@ -6,6 +6,20 @@ const nextConfig = {
   compiler: {
     styledComponents: true,
   },
+
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Cross-Origin-Opener-Policy",
+            value: "same-origin allow-popups",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
