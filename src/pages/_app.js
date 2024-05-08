2ab2ddda52993db8caaf1aec9d497b4bfd416893
jasonlin1993 <ninjachen.lin@gmail.com>
2024-05-08 17:20:50 +0800

    新增全局網頁標題

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,11 +1,15 @@
 // src/pages/_app.js
 import React from "react";
 import { Provider } from "react-redux";
+import Head from "next/head";
 import store from "../redux/store";
 
 const MyApp = ({ Component, pageProps }) => {
   return (
     <Provider store={store}>
+      <Head>
+        <title>GanttChart 線上甘特圖工具</title>
+      </Head>
       <Component {...pageProps} />
     </Provider>
   );
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,9 @@
 // src/pages/index.js
 import React from "react";
 import MainPage from "@/components/MainPage";
-import Head from "next/head";
 
 const Home = () => {
-  return (
-    <>
-      <Head>
-        <title>GanttChart 線上甘特圖工具</title>
-      </Head>
-      <MainPage />
-    </>
-  );
+  return <MainPage />;
 };
 
 export default Home;
