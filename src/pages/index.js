91eec643a6ecb0638f5ee0ad5fc56b48fa7618af
jasonlin1993 <ninjachen.lin@gmail.com>
2024-05-08 17:18:45 +0800

    新增網頁標題

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,17 @@
 // src/pages/index.js
 import React from "react";
 import MainPage from "@/components/MainPage";
+import Head from "next/head";
 
 const Home = () => {
-  return <MainPage />;
+  return (
+    <>
+      <Head>
+        <title>GanttChart 線上甘特圖工具</title>
+      </Head>
+      <MainPage />
+    </>
+  );
 };
 
 export default Home;
